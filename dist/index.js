5b59bfd675e46b380e32ac3809795d19b0f0fde3
Yoel <rls.cimof@gmail.com>
2025-07-31 17:06:03 +0300

    delete option

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,22 @@
 //Data
 var guitarsArray = [];
 var showForm = false;
+var idcounter = 0;
+var editGuitar = [];
 //view functions
 function htmlProduct(product) {
-    return "\n    <div class=\"product\">\n        <div class=\"product__img\"><img src=\"" + product.url + "\" alt=\"guitarImg\"></div>\n        <div class=\"product__name\">" + product.name + "</div> \n        <div class=\"product__price\">price: " + product.price + "$</div> \n        <div class=\"product__stock\">" + (product.amountinStock > 0 ? "In Stock: " + product.amountinStock : "Out Of Stock") + "</div>\n    </div>\n    ";
+    return "\n    <div class=\"product\">\n        <div class=\"product__img\"><img src=\"" + product.url + "\" alt=\"guitarImg\"></div>\n        <div class=\"product__name\">" + product.name + "</div> \n        <div class=\"product__price\">price: " + product.price + "$</div> \n        <div class=\"product__stock\">" + (product.amountinStock > 0
+        ? "In Stock: " + product.amountinStock
+        : "Out Of Stock") + "</div>\n        <div class=\"product__delete\" title=\"This will delete the item permanently!\"data-id =\"" + product.id + "\"><img src=\"./Images/trash.png\" alt=\"trashcan\"></div>\n        <div class=\"product__edit\" data-id =\"" + product.id + "\">EDIT ME</div>\n\n    </div>\n    ";
 }
 function renderProducts(guitars) {
     try {
         var guitarGrid = document.getElementById("guitarsRoot");
         if (!guitarGrid)
             throw new Error("guitarsRoot element not found");
-        guitarGrid.innerHTML = guitars.map(function (guitar) { return htmlProduct(guitar); }).join("");
+        guitarGrid.innerHTML = guitars
+            .map(function (guitar) { return htmlProduct(guitar); })
+            .join("");
     }
     catch (error) {
         console.error("error rendering guitars", error);
@@ -26,10 +32,10 @@ window.addEventListener("DOMContentLoaded", function () {
             showForm = !showForm;
             displayForm(showForm);
         });
-        var addForm = document.querySelector('.addGuitar');
+        var addForm = document.querySelector(".addGuitar");
         if (!addForm)
             throw new Error("Form not found");
-        addForm.addEventListener("submit", (handleSubmit));
+        addForm.addEventListener("submit", handleSubmit);
     }
     catch (error) {
         console.error("error events: ", error);
@@ -39,20 +45,23 @@ window.addEventListener("DOMContentLoaded", function () {
 function handleSubmit(event) {
     try {
         event.preventDefault(); //*
-        console.log('Form submitted');
+        console.log("Form submitted");
         if (!(event.target instanceof HTMLFormElement))
-            throw new Error('Event target is not a form');
+            throw new Error("Event target is not a form");
         var formData = new FormData(event.target); //*
         var data = Object.fromEntries(formData.entries()); //*
-        console.log('Form data:', data); //*
+        console.log("Form data:", data); //*
         var newProduct = {
             url: data.urlImage,
             name: data.name,
             amountinStock: parseInt(data.inStock, 10),
-            price: parseFloat(data.price)
+            price: parseFloat(data.price),
+            id: idcounter
         };
+        idcounter++;
         guitarsArray.push(newProduct);
         renderProducts(guitarsArray);
+        deleteButton();
         event.target.reset();
         console.dir(guitarsArray);
         showForm = false;
@@ -75,5 +84,23 @@ function displayForm(isVisible) {
     }
     catch (error) {
         console.error("error displayForm", error);
+    }
+}
+function deleteButton() {
+    var deleteButtons = document.querySelectorAll(".product__delete");
+    try {
+        if (!deleteButtons)
+            throw new Error("can't find an element to delete");
+        deleteButtons.forEach(function (button) {
+            var deleteId = Number(button.getAttribute("data-id"));
+            button.addEventListener("click", function (e) {
+                guitarsArray = guitarsArray.filter(function (guitar) { return guitar.id !== deleteId; });
+                renderProducts(guitarsArray);
+                deleteButton();
+            });
+        });
+    }
+    catch (error) {
+        console.error("can't find an element to delete", error);
     }
 }
